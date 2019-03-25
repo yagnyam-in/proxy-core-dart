29590e9ba9dda6e9b7ebf21b67975f6535d03c45
Chenna Reddy <chenna@alavala.in>
2019-03-25 15:08:43 +0100

    adding toFcmMap for alerts

--- lib/src/core/signable_alert_message.dart
+++ lib/src/core/signable_alert_message.dart
@@ -4,7 +4,21 @@ import 'signable_message.dart';
 
 abstract class SignableAlertMessage extends SignableMessage {
 
+  static const String ALERT_TYPE = "alertType";
+  static const String ALERT_ID = "alertId";
+
   String get alertId;
 
   List<ProxyId> get receivers;
+
+  /**
+   * Minimum required fields for Sending this message as FCM Message so that Client can act on it.
+   *
+   * @return Map of minimum required fields for this Alert.
+   */
+  Map<String, String> toFcmMap() => {
+    ALERT_TYPE: messageType,
+    ALERT_ID: alertId,
+  };
+
 }
