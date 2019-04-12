e3fc702b94c0dc81c9b4fdce2b3d03c5affd28b2
Chenna Reddy <chenna@alavala.in>
2019-04-12 08:53:49 +0200

    added missing services

--- lib/services.dart
+++ lib/services.dart
@@ -1,9 +1,14 @@
 export 'package:proxy_core/src/bootstrap/proxy_request_factory.dart';
-export 'src/services/remote_proxy_resolver.dart';
-export 'src/services/cryptography_service.dart';
+
 export 'src/services/proxy_resolver.dart';
+export 'src/services/remote_proxy_resolver.dart';
+export 'src/services/cached_proxy_resolver.dart';
+
+export 'src/services/cryptography_service.dart';
+
 export 'src/services/proxy_http_client.dart';
 export 'src/services/proxy_key_resolver.dart';
+
 export 'src/services/message_factory.dart';
 export 'src/services/message_signing_service.dart';
 export 'src/services/message_verification_service.dart';
