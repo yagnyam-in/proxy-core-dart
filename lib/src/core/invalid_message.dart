4dff3474ff950ee130f4a61856bb510ad6362257
Chenna Reddy <chenna@alavala.in>
2019-05-11 19:47:06 +0200

    meaningful exceptions

--- lib/src/core/invalid_message.dart
+++ lib/src/core/invalid_message.dart
@@ -5,5 +5,11 @@ class InvalidMessageException implements Exception {
 
   final Object invalidMessage;
 
-  const InvalidMessageException(this.errorMessage, [this.invalidMessage]);
+  const InvalidMessageException(this.errorMessage, [this.invalidMessage]) : super();
+
+  @override
+  String toString() {
+    return "InvalidMessageException: $errorMessage in $invalidMessage";
+  }
+
 }
