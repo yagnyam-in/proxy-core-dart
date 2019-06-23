7b0c7a83a875984e7b0a719d210700a529a47fc1
Chenna Reddy <chenna@alavala.in>
2019-06-23 17:14:06 +0200

    using latest sdk

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,7 +1,6 @@
 export 'src/bootstrap/proxy_request.dart';
 export 'src/bootstrap/proxy_request_factory.dart';
 export 'src/bootstrap/proxy_factory.dart';
-export 'src/bootstrap/proxy_key_store.dart';
 export 'src/bootstrap/proxy_key_factory.dart';
 export 'src/bootstrap/proxy_id_factory.dart';
 export 'src/bootstrap/messages/proxy_customer_update_request.dart';
--- lib/src/bootstrap/proxy_key_store.dart
+++
@@ -1,9 +0,0 @@
-import 'package:proxy_core/core.dart';
-import 'package:proxy_core/src/services/proxy_key_resolver.dart';
-
-import 'proxy_request.dart';
-
-/// Proxy Key Store
-abstract class ProxyKeyStore extends ProxyKeyResolver with ProxyUtils {
-  Future<void> saveProxy({ProxyKey proxyKey, Proxy proxy});
-}
